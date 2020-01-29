0f67b1d0ff60aff5664a7ca58808bf3012299adc
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-01-29 11:16:49 -0300

    initial project structure

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const server = require("./app");
+
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
