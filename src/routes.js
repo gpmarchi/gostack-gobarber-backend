acc6aada89984e89115bca4e2ab851518ea62daf
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-01-30 15:50:54 -0300

    user model route test

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Teste' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gustavo Pinto Marchi',
+    email: 'gustavomarchi@gmail.com',
+    password_hash: '12346789',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
