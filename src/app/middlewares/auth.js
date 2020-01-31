9c0382f2c00fefbc40f7bef9456329d8d985f969
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-01-31 17:55:39 -0300

    authentication middleware

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  const [, token] = authHeader.split(' ');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token not provided.' });
+  }
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid token provided.' });
+  }
+};
